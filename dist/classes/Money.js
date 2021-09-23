d441911dc1b3f2ee56b43c6498a9de7ddc077a2b
sherpa1 <alex@sherpa.one>
2021-09-23 12:59:45 +0200

    remove dist

--- dist/classes/Money.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Money {
-}
-exports.default = Money;
-//# sourceMappingURL=Money.js.map
--- dist/classes/Staff.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Staff {
-}
-exports.default = Staff;
-//# sourceMappingURL=Staff.js.map
