94709c7da126b8aee884ed493a48f904bd3ae60b
sherpa1 <alex@sherpa.one>
2021-10-01 22:09:42 +0200

    remove unsuded interface

--- src/classes/beverages/Beverage.ts
+++ src/classes/beverages/Beverage.ts
@@ -2,17 +2,14 @@ export default abstract class Beverage {
   protected _limit: number;
   protected _value: number;
   protected _name: string;
-  protected _volume: number;
   protected _price: number;
   private _water: number = 0;
-  public get water(): number {
-    return this._water;
-  }
 
   constructor(name: string, price: number, water: number = 0) {
     if (name === undefined || name === "")
       throw new Error(`name must be defined and not empty`);
-    if (water === undefined) throw new Error(`water must be defined`);
+    if (water === undefined || water <= 0)
+      throw new Error(`water must be defined and greater than 0`);
     if (price === undefined || price <= 0)
       throw new Error(`price must be defined and greater than 0`);
 
@@ -33,16 +30,6 @@ export default abstract class Beverage {
     return this._value;
   }
 
-  protected get volume(): number {
-    return this._volume;
-  }
-
-  protected set volume(value: number) {
-    if (value === undefined) throw new Error(`value must be defined`);
-
-    this._volume = value;
-  }
-
   public get price(): number {
     return this._price;
   }
@@ -50,5 +37,9 @@ export default abstract class Beverage {
   public set price(value: number) {
     if (value === undefined) throw new Error(`value must be defined`);
     this._price = value;
+  }
+
+  public get water(): number {
+    return this._water;
   }
 }
--- src/classes/interfaces/IMaintainable.ts
+++
@@ -1,3 +0,0 @@
-export default interface IMaintainable {
-  _quantity: number;
-}
